1aa9665e1a433ffbf1b1a5631738506ebf4ed61a
Felipe Botelho <felipebotelho18@gmail.com>
2022-09-16 09:41:01 -0300

    initial commit

---
+++ resources/views/components/base-layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        @vite('resources/css/app.css')
+        <title>{{ $title }} - Passou Ganhou</title>
+    </head>
+    <body class="antialiased">
+       <x-header />
+
+        <main>
+            {{ $main }}
+        </main>
+    </body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+<x-base-layout>
+    <x-slot name="title">Home</x-slot>
+    <x-slot name="main">
+        dasd
+    </x-slot>
+</x-base-layout>
