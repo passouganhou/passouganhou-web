6c90429269d22faf81ee22dd869ef37fc3060ecb
Felipe Botelho <felipebotelho18@gmail.com>
2022-09-20 09:12:33 -0300

    contact resource

---
+++ app/Filament/Resources/ContactResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContactResource\Pages;
+use App\Filament\Resources\ContactResource\RelationManagers;
+use App\Models\Contact;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ContactResource extends Resource
+{
+    protected static ?string $model = Contact::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Nome')
+                    ->disabled(),
+                TextInput::make('email')
+                    ->label('Email')
+                    ->disabled(),
+                TextInput::make('phone')
+                    ->label('Telefone')
+                    ->disabled(),
+                TextInput::make('mobileOptIn')
+                    ->label('Aceita receber comunicação por celular')
+                    ->disabled(),
+                TextInput::make('emailOptIn')
+                    ->label('Aceita receber comunicação por email')
+                    ->disabled(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->label('Nome'),
+                TextColumn::make('email')->label('Email'),
+                TextColumn::make('phone')->label('Telefone')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContacts::route('/'),
+            'create' => Pages\CreateContact::route('/create'),
+            'edit' => Pages\EditContact::route('/{record}/edit'),
+        ];
+    }
+}
