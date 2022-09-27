bd0fc0e2ed16c0072eb4b6b35274fa0cb6256f69
Felipe Botelho <felipebotelho18@gmail.com>
2022-09-27 10:13:05 -0300

    fix text btn-magenta

--- resources/views/components/btn-magenta.blade.php
+++ resources/views/components/btn-magenta.blade.php
@@ -7,7 +7,7 @@
     'bg-passou-magenta text-white inline-flex items-center justify-center rounded-full py-3 uppercase transition-all duration-200 group cursor-pointer',
     'hover:bg-white hover:text-passou-magenta' => !$bg,
     'hover:bg-passou-cyan hover:text-white' => $bg,
-    ]) }}">
+    ]) }}>
     <span class="mr-2">{{ $slot }}</span>
     {{ $icon ?? ''}}
     @if ($chevronRight)
