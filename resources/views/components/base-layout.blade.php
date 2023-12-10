b0994568db05dfa7b05f6cfd5f3ec89036c0ada0
locasmendes <lukinhazmm@gmail.com>
2023-12-10 14:43:55 -0300

    Inclusão do gtag

--- resources/views/components/base-layout.blade.php
+++ resources/views/components/base-layout.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
+        @production
+            @include('partials.gtag-head', ['id' => 'G-H3MNCV490M'])
+        @endproduction
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
