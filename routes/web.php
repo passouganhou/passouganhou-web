b4b9a59d4cb0da9dbc6086d31d1bc966d001cbdb
locasmendes <lukinhazmm@gmail.com>
2024-04-26 02:44:38 -0300

    Debug: RD params

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,7 @@ Route::get('politica-de-privacidade', function () {
 })->name('politica-de-privacidade');
 
 Route::middleware('auth')->group(function () {
-    Route::get('rd-debug', \App\Http\Controllers\Simulador\WebController::class, 'rdDebug');
+    Route::get('rd-debug', [\App\Http\Controllers\Simulador\WebController::class, 'rdDebug']);
     Route::post('export-contacts', [ExportController::class, 'export'])->name('export.contacts');
 });
 
