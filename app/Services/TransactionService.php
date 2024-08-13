f13cfba1ae23c4dfab70d02c7499d268a6b828c2
locasmendes <lukinhazmm@gmail.com>
2024-08-13 16:50:28 -0300

    API: ajuste nos TransactionService.php

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -13,46 +13,29 @@ class TransactionService
         $startDate = $this->formatDateTime($startDate, 'start');
         $endDate = $this->formatDateTime($endDate, 'end');
         return Transaction::where('status_category_description', 'Confirmada')
-            ->where('transaction_date', '>=', $startDate)
-            ->where('transaction_date', '<=', $endDate)
-            ->limit($limit)
+            //->where('transaction_date', '>=', $startDate)
+            //->where('transaction_date', '<=', $endDate)
+            //->limit($limit)
             ->get();
     }
 
-    public function getTransactionsGroupedByCustomerId($startDate, $endDate)
+    public function getTransactionsGroupedByCustomerId($startDate, $endDate, $limit = 1000)
     {
         $startDate = $this->formatDateTime($startDate, 'start');
         $endDate = $this->formatDateTime($endDate, 'end');
-        /*
-         * 'total_customers', 'total_transactions', 'total_amount', 'customers' => [customer_id => ['total_amount', 'transaction_count', 'most_recent_transaction_date', 'most_recent_transaction_amount']]
-         * */
-        $transactions = Transaction::select('customer_id', DB::raw('SUM(amount) as total_amount'), DB::raw('COUNT(*) as transaction_count'), DB::raw('MAX(transaction_date) as most_recent_transaction_date'), DB::raw('MAX(amount) as most_recent_transaction_amount'))
+        return Transaction::select(
+            'customer_id',
+            DB::raw('SUM(amount)/100 as total_amount'),
+            DB::raw('COUNT(*) as transaction_count'),
+            DB::raw('MAX(transaction_date) as last_transaction_date'),
+            DB::raw('NOW() as query_date'),
+            DB::raw('CASE WHEN MAX(transaction_date) < NOW() - INTERVAL 15 DAY THEN 1 ELSE 0 END as more_than_15_days')
+        )
             ->where('status_category_description', 'Confirmada')
-            ->where('transaction_date', '>=', $startDate)
-            ->where('transaction_date', '<=', $endDate)
             ->whereNotNull('customer_id')
             ->groupBy('customer_id')
             ->orderBy('total_amount', 'desc')
             ->get();
-
-        $totalCustomers = $transactions->count();
-        $totalTransactions = $transactions->sum('transaction_count');
-        $totalAmount = $transactions->sum('total_amount');
-        $customers = $transactions->mapWithKeys(function ($item) {
-            return [$item->customer_id => [
-                'total_amount' => $item->total_amount,
-                'transaction_count' => $item->transaction_count,
-                'most_recent_transaction_date' => $item->most_recent_transaction_date,
-                'most_recent_transaction_amount' => $item->most_recent_transaction_amount
-            ]];
-        });
-
-        return collect([
-            'total_customers' => $totalCustomers,
-            'total_transactions' => $totalTransactions,
-            'total_amount' => $totalAmount,
-            'data' => $customers
-        ]);
     }
 
     public function getMostValuableCustomers($startDate, $endDate): \Illuminate\Support\Collection
@@ -62,12 +45,12 @@ class TransactionService
         $customers = DB::table('gsurf_transactions')
             ->select('customer_id', DB::raw('SUM(amount) as total_amount'), DB::raw('COUNT(*) as transaction_count'))
             ->where('status_category_description', 'Confirmada')
-            ->where('transaction_date', '>=', $startDate)
-            ->where('transaction_date', '<=', $endDate)
+            //->where('transaction_date', '>=', $startDate)
+            //->where('transaction_date', '<=', $endDate)
             ->whereNotNull('customer_id')
             ->groupBy('customer_id')
             ->orderBy('total_amount', 'desc')
-            ->limit(100)
+            //->limit(100)
             ->get();
 
         return $customers;
@@ -81,12 +64,12 @@ class TransactionService
         $customers = DB::table('gsurf_transactions')
             ->select('customer_id', DB::raw('SUM(amount) as total_amount'), DB::raw('COUNT(*) as transaction_count'))
             ->where('status_category_description', 'Confirmada')
-            ->where('transaction_date', '>=', $startDate)
-            ->where('transaction_date', '<=', $endDate)
+            //->where('transaction_date', '>=', $startDate)
+            //->where('transaction_date', '<=', $endDate)
             ->whereNotNull('customer_id')
             ->groupBy('customer_id')
             ->orderBy('total_amount', 'asc')
-            ->limit(100)
+            //->limit(100)
             ->get();
 
         return $customers;
@@ -99,8 +82,8 @@ class TransactionService
         $result = DB::table('gsurf_transactions')
             ->select(DB::raw('COUNT(*) as total_transactions'), DB::raw('SUM(amount) as total_amount'))
             ->where('status_category_description', 'Confirmada')
-            ->where('transaction_date', '>=', $startDate)
-            ->where('transaction_date', '<=', $endDate)
+            //->where('transaction_date', '>=', $startDate)
+            //->where('transaction_date', '<=', $endDate)
             ->first();
 
         return $result;
